a2caa931466de29575b7c88a58a9c4a1c117759a
manojpraba <mprabakaran792@gmail.com>
2023-11-03 19:37:35 +0530

    local_df_added

--- data_dump.py
+++ data_dump.py
@@ -2,7 +2,7 @@ import pymongo
 import pandas as pd
 import json
 # Provide the mongodb localhost url to connect python to mongodb.
-client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
+client = pymongo.MongoClient("mongodb+srv://mprabakaran792:root@cluster0.dh07kzz.mongodb.net/?retryWrites=true&w=majority")
 
 DATA_FILE_PATH="/config/workspace/aps_failure_training_set1.csv"
 DATABASE_NAME="aps"
--- sensor/utils/__init__.py
+++ sensor/utils/__init__.py
@@ -15,12 +15,13 @@ def get_collection_as_dataframe(database_name:str,collection_name:str)->pd.DataF
     return Pandas dataframe of a collection
     """
     try:
-        logging.info(f"Reading data from database: {database_name} and collection: {collection_name}")
-        df = pd.DataFrame(list(mongo_client[database_name][collection_name].find()))
-        logging.info(f"Found columns: {df.columns}")
-        if "_id" in df.columns:
-            logging.info(f"Dropping column: _id ")
-            df = df.drop("_id",axis=1)
+        # logging.info(f"Reading data from database: {database_name} and collection: {collection_name}")
+        # df = pd.DataFrame(list(mongo_client[database_name][collection_name].find()))
+        # logging.info(f"Found columns: {df.columns}")
+        # if "_id" in df.columns:
+        #     logging.info(f"Dropping column: _id ")
+        df=pd.read_csv('C:\Ml projects\aps-fault-detection-3ed0c23237711fcb380d479a6b734af781624404\aps-fault-detection-3ed0c23237711fcb380d479a6b734af781624404\aps_failure_training_set1.csv')
+        df = df.drop("_id",axis=1)
         logging.info(f"Row and columns in df: {df.shape}")
         return df
     except Exception as e:
